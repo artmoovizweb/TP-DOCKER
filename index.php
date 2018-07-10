c1a78479ce359548d6bb106b5515c85f6c6bfca1
artmoovizweb <32265875+artmoovizweb@users.noreply.github.com>
2018-07-10 07:58:47 +0200

    devoir 01

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+ $title = 'Site docker d\'exemple 1';
+?>
+
+<!Doctype html>
+<html>
+ <head>
+ <meta charset="utf-8">
+ <title><?= $title ?></title>
+ </head>
+ <body>
+ <h1><?= $title ?></h1>
+ </body>
+</html>
